179b7c393ab424425671b22159c2ddfa8963cc13
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-06-16 00:03:08 +0100

    Initial Commit

---
+++ Java back end/src/main/java/com/codeclan/example/Gym/models/Member.java
@@ -0,0 +1,20 @@
+package com.codeclan.example.Gym.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "members")
+
+
+public class Member {
+
+
+    private String firstName;
+
+    private String lastName;
+
+
+
+
+}

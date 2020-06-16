75f8286401533aa356bcf42ded9b65fae76dca54
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-06-16 12:06:43 +0100

    Member Class created

--- Java back end/src/main/java/com/codeclan/example/Gym/models/Member.java
+++ Java back end/src/main/java/com/codeclan/example/Gym/models/Member.java
@@ -1,20 +1,63 @@
 package com.codeclan.example.Gym.models;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "members")
 
-
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "firstName")
     private String firstName;
 
+    @Column(name = "lastName")
     private String lastName;
 
+    @Column(name = "goal")
+    private String goal;
 
+    public Member(String firstName, String lastName, String goal) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.goal = goal;
+    }
 
+    public Member(){
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
 }

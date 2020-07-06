834fad0b818c7c87f6691ceb6537665b441b1385
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-07-06 15:41:23 +0100

    Post Member Request Passed - Tested in Insomnia

--- Java back end/src/main/java/com/codeclan/example/Gym/controllers/MemberController.java
+++ Java back end/src/main/java/com/codeclan/example/Gym/controllers/MemberController.java
@@ -5,9 +5,7 @@ import com.codeclan.example.Gym.repositories.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,17 @@ public class MemberController {
     public ResponseEntity<List<Member>>getAllMembers(){
         return new ResponseEntity<>(memberRepository.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Member> getMember(@PathVariable Long id) {
+        return new ResponseEntity(memberRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Member> postMember(@RequestBody Member member) {
+        memberRepository.save(member);
+        return new ResponseEntity<>(member, HttpStatus.CREATED);
+    }
+
+
 }

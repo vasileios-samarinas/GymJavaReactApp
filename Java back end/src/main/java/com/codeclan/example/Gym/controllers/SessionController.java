e91ab5d3b0e9e5d76308e93d124395fe463f35ef
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-07-06 16:49:59 +0100

    SessionController requests completed - Tested with Insomnia

--- Java back end/src/main/java/com/codeclan/example/Gym/controllers/SessionController.java
+++ Java back end/src/main/java/com/codeclan/example/Gym/controllers/SessionController.java
@@ -1,15 +1,11 @@
 package com.codeclan.example.Gym.controllers;
-
-
-import com.codeclan.example.Gym.models.Member;
+import com.codeclan.example.Gym.models.Room;
 import com.codeclan.example.Gym.models.Session;
 import com.codeclan.example.Gym.repositories.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +16,37 @@ public class SessionController {
     @Autowired
     SessionRepository sessionRepository;
 
+//  READ - GET ALL SESSIONS
     @GetMapping
     public ResponseEntity<List<Session>> getAllSessions(){
         return new ResponseEntity<>(sessionRepository.findAll(), HttpStatus.OK);
     }
+
+    //  SHOW ONE SESSION
+    @GetMapping(value="/{id}")
+    public ResponseEntity<List<Session>>getSession(@PathVariable Long id){
+        return new ResponseEntity(sessionRepository.findById(id), HttpStatus.OK);
+    }
+
+    //  CREATE ONE SESSION
+    @PostMapping
+    public ResponseEntity<Session> postSession(@RequestBody Session session){
+        sessionRepository.save(session);
+        return new ResponseEntity(session, HttpStatus.CREATED);
+    }
+
+    //  UPDATE ONE SESSION
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Session> putSession(@RequestBody Session session){
+        sessionRepository.save(session);
+        return new ResponseEntity(session, HttpStatus.CREATED);
+    }
+
+    //  DELETE ONE SESSION
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Session> deleteSession(@PathVariable Long id){
+        sessionRepository.deleteById(id);
+        return new ResponseEntity(id,HttpStatus.OK);
+    }
+
 }

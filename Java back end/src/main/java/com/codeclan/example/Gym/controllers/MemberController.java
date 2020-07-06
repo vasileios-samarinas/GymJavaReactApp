ffcb7bc7ed4325827a6872e3c19e15e11eb77cb0
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-07-06 16:03:52 +0100

    Update one member Request passed - Tested with Insomnia

--- Java back end/src/main/java/com/codeclan/example/Gym/controllers/MemberController.java
+++ Java back end/src/main/java/com/codeclan/example/Gym/controllers/MemberController.java
@@ -16,20 +16,29 @@ public class MemberController {
     @Autowired
     MemberRepository memberRepository;
 
+//  READ - ALL MEMBERS
     @GetMapping
     public ResponseEntity<List<Member>>getAllMembers(){
         return new ResponseEntity<>(memberRepository.findAll(), HttpStatus.OK);
     }
-
+//  SHOW ONE MEMBER
     @GetMapping(value="/{id}")
     public ResponseEntity<Member> getMember(@PathVariable Long id) {
         return new ResponseEntity(memberRepository.findById(id), HttpStatus.OK);
     }
 
+//  CREATE ONE MEMBER
     @PostMapping
     public ResponseEntity<Member> postMember(@RequestBody Member member) {
         memberRepository.save(member);
         return new ResponseEntity<>(member, HttpStatus.CREATED);
+    }
+
+//  UPDATE ONE MEMBER
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Member> putMember(@RequestBody Member member){
+        memberRepository.save(member);
+        return new ResponseEntity<>(member, HttpStatus.OK);
     }
 
 

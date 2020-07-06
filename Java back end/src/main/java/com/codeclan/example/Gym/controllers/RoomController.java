6862f6b3f3a14f08a95a5f529990d6b823eeedb0
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-07-06 16:24:19 +0100

    Create one room request completed - Tested with Insomnia

--- Java back end/src/main/java/com/codeclan/example/Gym/controllers/RoomController.java
+++ Java back end/src/main/java/com/codeclan/example/Gym/controllers/RoomController.java
@@ -7,10 +7,7 @@ import com.codeclan.example.Gym.repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +28,12 @@ public class RoomController {
     @GetMapping(value="/{id}")
     public ResponseEntity<List<Room>>getRoom(@PathVariable Long id){
         return new ResponseEntity(roomRepository.findById(id), HttpStatus.OK);
+    }
+
+//  CREATE ONE ROOM
+    @PostMapping
+    public ResponseEntity<Room> postRoom(@RequestBody Room room){
+        roomRepository.save(room);
+        return new ResponseEntity(room, HttpStatus.CREATED);
     }
 }
